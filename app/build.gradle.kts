56772636e6477fcfef4fb3e751a35323f07e8438
J Herskowitz <jherskowitz@globallistic.com>
2026-04-26 10:29:24 -0400

    Release v0.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.parachord.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.4.0-beta.2"
+        versionCode = 6
+        versionName = "0.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
